e798d64acb30669c3fb50e5fd823abe09512b223
Daniel Ritter <danieldritter1@gmail.com>
2022-06-28 12:03:56 +0100

    Updating cluster script

--- cluster_script.sh
+++ cluster_script.sh
@@ -12,5 +12,7 @@ export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs
 /scratch-ssd/oatml/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f environment.yml
 source /scratch-ssd/oatml/miniconda3/bin/activate ms21ddr_llms
 
-rm -rf "/scratch-ssd/ms21ddr/data/hf_language_datasets/spurious_sst"
-# srun python train_models.py with 'run_name="dn_t5_base_enc/spurious_sst/cls-finetune"' 'data_cache_dir="/scratch-ssd/ms21ddr/data/hf_language_datasets"' 'disable_tqdm=True'
+srun python train_models.py with 'run_name="dn_t5_tiny_enc/spurious_sst/cls-finetune"' 'data_cache_dir="/scratch-ssd/ms21ddr/data/hf_language_datasets"' 'disable_tqdm=True'
+srun python train_models.py with 'run_name="dn_t5_mini_enc/spurious_sst/cls-finetune"' 'data_cache_dir="/scratch-ssd/ms21ddr/data/hf_language_datasets"' 'disable_tqdm=True'
+srun python train_models.py with 'run_name="dn_t5_small_enc/spurious_sst/cls-finetune"' 'data_cache_dir="/scratch-ssd/ms21ddr/data/hf_language_datasets"' 'disable_tqdm=True'
+srun python train_models.py with 'run_name="dn_t5_base_enc/spurious_sst/cls-finetune"' 'data_cache_dir="/scratch-ssd/ms21ddr/data/hf_language_datasets"' 'disable_tqdm=True'
