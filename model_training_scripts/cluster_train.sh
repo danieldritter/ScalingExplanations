09ea788eafa775c3a6cc1aeeff3a4668039287a7
Daniel Ritter <danieldritter1@gmail.com>
2022-08-03 16:00:29 +0100

    script fix

--- model_training_scripts/cluster_train.sh
+++ model_training_scripts/cluster_train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:3g.40gb:1
 
 #SBATCH --output=slurm-%j.out
@@ -9,7 +9,7 @@
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
 export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs
 
-/scratch-ssd/oatml/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f environment.yml
+/scratch-ssd/oatml/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f ../environment.yml
 source /scratch-ssd/oatml/miniconda3/bin/activate ms21ddr_llms
 
 # srun python model_training_scripts/train_models.py with 'run_name="dn_t5_tiny_enc/eraser_esnli/avg-finetune"' 'data_cache_dir="/scratch-ssd/ms21ddr/data/hf_language_datasets"' 'disable_tqdm=True' 'output_dir="/scratch-ssd/ms21ddr/model_outputs"'
