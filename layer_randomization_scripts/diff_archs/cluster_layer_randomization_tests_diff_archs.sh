67fe441d994b36a27cc703f44714f4714df4029f
Daniel Ritter <danieldritter1@gmail.com>
2022-08-07 04:45:12 +0100

    script update

--- layer_randomization_scripts/diff_archs/cluster_layer_randomization_tests_diff_archs.sh
+++ layer_randomization_scripts/diff_archs/cluster_layer_randomization_tests_diff_archs.sh
@@ -29,11 +29,11 @@ function run_explanation_set {
             echo "CHECKPOINT_FOLDER: ${checkpoint_folders[j]}"
             EXP_MAX_IND="$((${#explanations[@]} - 1))"
             if [ "$i" -eq "$EXP_MAX_IND" ]; then
-                srun python layer_randomization.py with "explanation_type=${explanations[i]}" "output_folder=${4}" \
+                srun python ./layer_randomization_scripts/layer_randomization.py with "explanation_type=${explanations[i]}" "output_folder=${4}" \
                 "num_examples=$5" seed=${10} "checkpoint_folder=${checkpoint_folders[j]}" "run_name=${run_names[j]}" "save_examples=True" \
                 "data_cache_dir=$6" "layer=${layers[j]}" "cascading=$8" "num_layers=${num_layers[j]}" "layer_object=${layer_objects[j]}"
             else 
-                srun python layer_randomization.py with "explanation_type=${explanations[i]}" "output_folder=${4}" \
+                srun python ./layer_randomization_scripts/layer_randomization.py with "explanation_type=${explanations[i]}" "output_folder=${4}" \
                 "num_examples=$5" seed=${10} "checkpoint_folder=${checkpoint_folders[j]}" "run_name=${run_names[j]}" \
                 "data_cache_dir=${6}" "layer=${layers[j]}" "cascading=$8" "num_layers=${num_layers[j]}" "layer_object=${layer_objects[j]}"
             fi 
@@ -47,21 +47,22 @@ function run_explanation_set {
 
 SEED=765
 EXPLANATIONS=('gradients/gradients_x_input' 'gradients/gradients' \
-'gradients/integrated_gradients_x_input' 'gradients/integrated_gradients' \
-'lime/lime' 'shap/shap' 'attention/attention_rollout' 'random/random_baseline')
+'gradients/integrated_gradients_x_input' \
+'lime/lime' 'shap/shap' 'attention/attention_rollout' 'attention/average_attention' 'random/random_baseline')
 
-OUTPUT_FOLDER='./dn_layer_randomization_outputs_diff_archs'
+OUTPUT_FOLDER='./diff_archs_layer_randomization_50'
 CACHE_DIR="/scratch-ssd/ms21ddr/data/hf_language_datasets"
 LAYER_OBJECTS=( "encoder.block" "transformer.h" "roberta.encoder.layer" "bert.encoder.layer" )
-NUM_EXAMPLES=15
+NUM_EXAMPLES=50
 LAYERS=( 'encoder.embed_tokens' 'transformer.wte' 'roberta.embeddings.word_embeddings' 'bert.embeddings.word_embeddings' )
 NUM_LAYERS=( 12 12 12 12 )
 
 
-RUN_NAMES=( 't5_base_enc/spurious_sst/cls-finetune' 'gpt2_small/spurious_sst/cls-finetune' \
+RUN_NAMES=( 't5_base_enc/spurious_sst/avg-finetune' 'gpt2_small/spurious_sst/cls-finetune' \
 'roberta_base/spurious_sst/cls-finetune' 'bert_base_uncased/spurious_sst/cls-finetune')
-CHECKPOINT_FOLDERS=( './model_outputs/t5_base_enc/spurious_sst/cls-finetune/checkpoint-25260' \
-'./model_outputs/gpt2_small/spurious_sst/cls-finetune/checkpoint-25260' \
+
+CHECKPOINT_FOLDERS=( './model_outputs/t5_base_enc/spurious_sst/avg-finetune/checkpoint-25260' \
+'./model_outputs/gpt2_small/spurious_sst/cls-finetune/checkpoint-101028' \
 './model_outputs/roberta_base/spurious_sst/cls-finetune/checkpoint-25260' \
 './model_outputs/bert_base_uncased/spurious_sst/cls-finetune/checkpoint-25260')
 
@@ -74,11 +75,11 @@ $OUTPUT_FOLDER $NUM_EXAMPLES $CACHE_DIR "${LAYERS[*]}" $CASCADING "${NUM_LAYERS[
 
 echo "SPURIOUS_SST EXPLANATIONS COMPLETED"
 
-RUN_NAMES=( 't5_base_enc/mnli/cls-finetune' 'gpt2_small/mnli/cls-finetune' \
+RUN_NAMES=( 't5_base_enc/mnli/avg-finetune' 'gpt2_small/mnli/cls-finetune' \
 'roberta_base/mnli/cls-finetune' 'bert_base_uncased/mnli/cls-finetune')
 
-CHECKPOINT_FOLDERS=( './model_outputs/t5_base_enc/mnli/cls-finetune/checkpoint-122720' \
-'./model_outputs/gpt2_small/mnli/cls-finetune/checkpoint-245440' \
+CHECKPOINT_FOLDERS=( './model_outputs/t5_base_enc/mnli/avg-finetune/checkpoint-49088' \
+'./model_outputs/gpt2_small/mnli/cls-finetune/checkpoint-883584' \
 './model_outputs/roberta_base/mnli/cls-finetune/checkpoint-171808' \
 './model_outputs/bert_base_uncased/mnli/cls-finetune/checkpoint-196352')
 
@@ -90,3 +91,20 @@ run_explanation_set "${EXPLANATIONS[*]}" "${RUN_NAMES[*]}" "${CHECKPOINT_FOLDERS
 $OUTPUT_FOLDER $NUM_EXAMPLES $CACHE_DIR "${LAYERS[*]}" $CASCADING "${NUM_LAYERS[*]}" $SEED "${LAYER_OBJECTS[*]}"
 
 echo "MNLI EXPLANATIONS COMPLETED"
+
+RUN_NAMES=( 't5_base_enc/eraser_esnli/avg-finetune' 'gpt2_small/eraser_esnli/cls-finetune' \
+'roberta_base/eraser_esnli/cls-finetune' 'bert_base_uncased/eraser_esnli/cls-finetune')
+
+CHECKPOINT_FOLDERS=( './model_outputs/t5_base_enc/eraser_esnli/avg-finetune/checkpoint-137328' \
+'./model_outputs/gpt2_small/eraser_esnli/cls-finetune/checkpoint-961296' \
+'./model_outputs/roberta_base/eraser_esnli/cls-finetune/checkpoint-308988' \
+'./model_outputs/bert_base_uncased/eraser_esnli/cls-finetune/checkpoint-171660')
+
+CASCADING="False"
+run_explanation_set "${EXPLANATIONS[*]}" "${RUN_NAMES[*]}" "${CHECKPOINT_FOLDERS[*]}" \
+$OUTPUT_FOLDER $NUM_EXAMPLES $CACHE_DIR "${LAYERS[*]}" $CASCADING "${NUM_LAYERS[*]}" $SEED "${LAYER_OBJECTS[*]}"
+CASCADING="True"
+run_explanation_set "${EXPLANATIONS[*]}" "${RUN_NAMES[*]}" "${CHECKPOINT_FOLDERS[*]}" \
+$OUTPUT_FOLDER $NUM_EXAMPLES $CACHE_DIR "${LAYERS[*]}" $CASCADING "${NUM_LAYERS[*]}" $SEED "${LAYER_OBJECTS[*]}"
+
+echo "ERASER ESNLI EXPLANATIONS COMPLETED"
